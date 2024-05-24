3729f8e37a05a98161bb3f3fb3cf8dbacaa25e7e
Thao Ha <thao.ha@asnet.com.vn>
2024-05-24 16:06:51 +0700

    feat: add example for two-way-binding

--- examples/src/app/app.component.ts
+++ examples/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SizerComponent } from './sizer/sizer.component';
+import { TodoList } from './components/todo-list/todo-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [RouterOutlet, SizerComponent, TodoList],
 })
 export class AppComponent {
   title = 'examples';
+  fontSizePx = 16;
 }
