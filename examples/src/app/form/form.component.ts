58ffb030ba2746bb5bbc7113ccc0a26971b08cf3
Thao Ha <thao.ha@asnet.com.vn>
2024-05-29 18:34:00 +0700

    feat: Validating form input

--- examples/src/app/form/form.component.ts
+++ examples/src/app/form/form.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   standalone: true,
@@ -8,25 +15,29 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
       <label>
         Name
-        <input type="text" formControlName="name" />
+        <input type="text" formControlName="name" id="name" name="name" />
       </label>
+
+      <div *ngIf="profileForm.get('name')?.hasError('required')">
+        Name is required.
+      </div>
       <label>
         Email
         <input type="email" formControlName="email" />
       </label>
-      <button type="submit">Submit</button>
+      <button type="submit" [disabled]="!profileForm.valid">Submit</button>
     </form>
 
     <h2>Profile Form</h2>
     <p>Name: {{ profileForm.value.name }}</p>
     <p>Email: {{ profileForm.value.email }}</p>
   `,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
 })
 export class AppProfileEditor {
   profileForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
 
   handleSubmit() {
