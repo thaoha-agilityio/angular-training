2d92d48702e77d81eeb03d7a368108c73902b8e0
Thao Ha <thao.ha@asnet.com.vn>
2024-06-04 18:19:43 +0700

    feat: implement button component

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './shared/components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
