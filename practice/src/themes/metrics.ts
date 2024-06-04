5f84f0f44dfbc08c7a67350613930fcba7d451d1
Thao Ha <thao.ha@asnet.com.vn>
2024-06-04 15:11:18 +0700

    feat: define themes

---
+++ practice/src/themes/metrics.ts
@@ -0,0 +1,24 @@
+export const fontSizes = {
+  base: '14px',
+  md: '16px',
+};
+
+export const fontWeights = {
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+};
+
+export const lineHeights = {
+  md: '25px',
+  lg: '32px',
+  xl: '56px',
+};
+
+export const borderRadius = {
+  xs: '2px',
+  sm: '6px',
+  md: '8px',
+};
--- practice/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/app/*.{html,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
