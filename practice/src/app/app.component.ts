f57df8bb4a1c70ea6cabb63ea0841c525044402c
Thao Ha <thao.ha@asnet.com.vn>
2024-06-05 10:24:37 +0700

    feat: update class attribute

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from './shared/components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'practice';
-}
+export class AppComponent {}
--- practice/src/app/shared/components/button/button.component.ts
+++ practice/src/app/shared/components/button/button.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgClass } from '@angular/common';
 
+export type ButtonVariant = 'primary' | 'second' | 'tertiary';
+export type ButtonSize = 'small' | 'medium' | 'lager' | 'auto';
 @Component({
   selector: 'app-button',
   standalone: true,
@@ -10,10 +12,21 @@ import { NgClass } from '@angular/common';
 })
 export class ButtonComponent {
   @Input() label!: string;
-  @Input() variant!: string;
-  @Output() onClick = new EventEmitter();
+  @Input() variant!: ButtonVariant;
+  @Input() disabled: boolean = false;
+  @Input() size: ButtonSize = 'auto';
+  @Output() onClick = new EventEmitter<void>();
 
   onClickButton() {
     this.onClick.emit();
+  }
+
+  getClasses(): string {
+    return [
+      'flex items-center justify-center rounded-xs text-sm px-4 py-1 hover:opacity-70',
+      `btn--${this.variant}`,
+      `btn--${this.size}`,
+      this.disabled ? 'opacity-70 pointer-events-none' : 'cursor-pointer',
+    ].join(' ');
   }
 }
