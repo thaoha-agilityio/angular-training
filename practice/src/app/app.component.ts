41648c99b488dcc52f28f58d407f2d522ed5e547
Thao Ha <thao.ha@asnet.com.vn>
2024-06-05 17:59:01 +0700

    feat: remove redundant code

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -1,34 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BaseModalComponent } from './shared/components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BaseModalComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  isLoading: boolean = false;
-  description = 'description';
-  textCancel = 'cancel';
-  textSubmit = 'submit';
-  title = 'title';
-
-  constructor() {}
-
-  closeModal() {
-    // Handle close modal logic
-  }
-
-  submitModal() {
-    // Handle submit modal logic
-    this.isLoading = true;
-    // Perform any async task here
-    setTimeout(() => {
-      this.isLoading = false;
-      // Additional logic after async task completes
-    }, 2000);
-  }
-}
+export class AppComponent {}
