d76b8072d99d12016b40f2ce0c3c21c18cdf02a5
thao.ha <thao.ha@asnet.com.vn>
2024-06-09 16:52:13 +0700

    feat: add search icon

---
+++ practice/src/app/shared/icons/search-icon/search-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-icon.component.html',
+})
+export class SearchIconComponent {}
