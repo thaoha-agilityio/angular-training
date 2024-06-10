fb8d232bd24ad862e50d19d96133db3923b63a75
Thao Ha <thao.ha@asnet.com.vn>
2024-06-10 17:11:55 +0700

    feat: add icon components

---
+++ practice/src/app/shared/icons/list-icon/list-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-icon.component.html',
+})
+export class ListIconComponent {}
