88798ed0938277ec8b32ad51031a7fa6e00859b5
Thao Ha <thao.ha@asnet.com.vn>
2024-06-11 15:54:03 +0700

    feat: implement menu component

--- practice/src/app/shared/components/index.ts
+++ practice/src/app/shared/components/index.ts
@@ -3,5 +3,6 @@ export * from './base-modal/base-modal.component';
 export * from './button/button.component';
 export * from './detail-modal/detail-modal.component';
 export * from './input/input.component';
+export * from './menu/menu.component';
 export * from './search-input/search-input.component';
 export * from './table/table/table.component';
