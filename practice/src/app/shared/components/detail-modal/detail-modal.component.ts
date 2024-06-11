c2cdac5f50be238d1225ac608bc7e47065e53cb6
Thao Ha <thao.ha@asnet.com.vn>
2024-06-11 09:52:10 +0700

    fix: typescript

--- practice/src/app/shared/components/detail-modal/detail-modal.component.ts
+++ practice/src/app/shared/components/detail-modal/detail-modal.component.ts
@@ -1,17 +1,24 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 // Components
 import { EditIconComponent } from '../../icons';
 
+// Constants
+import { USER_STATUS } from '@/app/core/constants';
+
 @Component({
   selector: 'app-detail-modal',
   standalone: true,
-  imports: [EditIconComponent],
+  imports: [EditIconComponent, NgIf],
   templateUrl: './detail-modal.component.html',
   styleUrl: './detail-modal.component.css',
 })
 export class DetailModalComponent {
+  activeStatus = USER_STATUS.ACTIVE;
+
   @Input() title!: string;
+  @Input() status?: USER_STATUS;
 
   @Output() closeModal: EventEmitter<void> = new EventEmitter<void>();
 
--- practice/src/app/user/components/user-detail/user-detail.component.ts
+++ practice/src/app/user/components/user-detail/user-detail.component.ts
@@ -32,8 +32,7 @@ import { getFirstLetter, nameToColorHex } from '@/app/core/utils';
 export class UserDetailComponent implements OnChanges {
   variant: AvatarVariant = 'secondary';
   title = 'User information';
-  bgColor: string = '';
-  firstLetter: string = '';
+  bgColor!: string;
 
   @Input() userDetail!: User;
   @Output() closeDetailModal: EventEmitter<void> = new EventEmitter<void>();
